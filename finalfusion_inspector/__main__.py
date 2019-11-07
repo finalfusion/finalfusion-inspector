6eabfbd963b128bd0b0a15863dc8f5bea9d52c60
Daniël de Kok <me@danieldk.eu>
2019-11-07 09:38:26 +0100

    Show an open dialog if no embeddings were provided as an argument

--- finalfusion_inspector/__main__.py
+++ finalfusion_inspector/__main__.py
@@ -4,7 +4,7 @@ import argparse
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMessageBox
 import finalfusion
 
 from finalfusion_inspector.inspectorwindow import InspectorWindow
@@ -14,20 +14,38 @@ parser = argparse.ArgumentParser(description='Inspect finalfusion embeddings.')
 parser.add_argument(
     'embeddings',
     metavar='EMBEDS',
+    nargs='?',
     help='the embeddings to inspect')
+
+
+def openEmbeddingsDialog():
+    embeddingsFile, _ = QFileDialog.getOpenFileName(
+        caption="Open embeddings file", filter="Finalfusion Embeddings (*.fifu)")
+    if embeddingsFile == '':
+        print("No filename selected", file=sys.stderr)
+        sys.exit(1)
+
+    return embeddingsFile
 
 
 def main():
     args = parser.parse_args()
 
-    try:
-        embeddings = finalfusion.Embeddings(args.embeddings, mmap=True)
-    except Exception as e:
-        print("Cannot load embeddings from %s: %s" % (args.embeddings, e))
-        sys.exit(1)
-
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     app = QApplication(sys.argv)
+
+    embeddingsFile = args.embeddings
+    if embeddingsFile is None:
+        embeddingsFile = openEmbeddingsDialog()
+
+    try:
+        embeddings = finalfusion.Embeddings(embeddingsFile, mmap=True)
+    except Exception as e:
+        QMessageBox.critical(
+            None, "Cannot load embeddings", "Cannot load embeddings from %s: %s" %
+            (embeddingsFile, e))
+        sys.exit(1)
+
     window = InspectorWindow(embeddings)
     window.show()
 
