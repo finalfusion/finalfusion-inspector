08c2b679009bbc2542aa6ce29fedb60336e4bca4
Daniël de Kok <me@danieldk.eu>
2019-11-12 20:50:32 +0100

    First rough implementation of the similarity widget

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,72 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::rc::Rc;
+
+use clap::{App, AppSettings, Arg};
+use finalfusion::prelude::*;
 use gio::prelude::*;
 use gtk::prelude::*;
-
-use std::env::args;
+use stdinout::OrExit;
 
 #[macro_use]
 pub mod clone;
 
 mod inspector_window;
-use inspector_window::InspectorWindow;
+pub use inspector_window::InspectorWindow;
 
-fn build_ui(application: &gtk::Application) {
-    let window = InspectorWindow::new(application);
+pub mod model;
+
+pub mod similarity;
+
+mod similarity_widget;
+pub use similarity_widget::SimilarityWidget;
+
+static EMBEDDINGS: &str = "EMBEDDINGS";
+
+static DEFAULT_CLAP_SETTINGS: &[AppSettings] = &[
+    AppSettings::DontCollapseArgsInUsage,
+    AppSettings::UnifiedHelpMessage,
+];
+
+fn build_ui(
+    application: &gtk::Application,
+    embeddings: Rc<Embeddings<VocabWrap, StorageViewWrap>>,
+) {
+    let window = InspectorWindow::new(application, embeddings);
     window.window().show_all();
 }
 
 fn main() {
+    let app = App::new("finalfusion-inspector")
+        .settings(DEFAULT_CLAP_SETTINGS)
+        .arg(
+            Arg::with_name(EMBEDDINGS)
+                .help("Embedding file")
+                .index(1)
+                .required(true),
+        );
+
+    let matches = app.get_matches();
+    let embeddings_filename = matches.value_of(EMBEDDINGS).unwrap();
+
     let application =
         gtk::Application::new(Some("com.github.finalfusion.inspector"), Default::default())
             .expect("Initialization failed...");
 
-    application.connect_activate(|app| {
-        build_ui(app);
+    let mut read = BufReader::new(File::open(embeddings_filename).or_exit(
+        format!("Cannot read embeddings from {}", embeddings_filename),
+        1,
+    ));
+    let embeddings: Embeddings<VocabWrap, StorageViewWrap> = Embeddings::mmap_embeddings(&mut read)
+        .or_exit(
+            format!("Cannot read embeddings from {}", embeddings_filename),
+            1,
+        );
+    let embeddings = Rc::new(embeddings);
+
+    application.connect_activate(move |app| {
+        build_ui(app, embeddings.clone());
     });
 
-    application.run(&args().collect::<Vec<_>>());
+    application.run(&[]);
 }
---
+++ src/similarity_widget.rs
@@ -0,0 +1,93 @@
+use std::rc::Rc;
+
+use gtk::prelude::*;
+use gtk::{Box, Builder, Button, Entry, IconSize, Image, TreeView};
+
+use crate::model::{EmbeddingsExt, EmbeddingsModel, WordStatus};
+use crate::similarity::SimilarityModel;
+
+pub struct SimilarityWidget {
+    button: Button,
+    entry: Entry,
+    inner: Box,
+    model: Rc<SimilarityModel>,
+    status_image: Image,
+}
+
+impl SimilarityWidget {
+    pub fn new(model: SimilarityModel) -> Rc<Self> {
+        let model = Rc::new(model);
+
+        let glade_src = include_str!("similarity_widget.glade");
+        let builder = Builder::new_from_string(glade_src);
+
+        let widget: Box = builder
+            .get_object("similarityWidget")
+            .expect("Glade source is missing similarityWidget");
+
+        let view: TreeView = builder
+            .get_object("similarityView")
+            .expect("Glade source is missing similarityView");
+        view.set_model(Some(&model.model()));
+
+        let entry: Entry = builder
+            .get_object("queryEntry")
+            .expect("Glade source is missing queryEntry");
+
+        let button: Button = builder
+            .get_object("queryButton")
+            .expect("Glade source is missing queryButton");
+
+        let status_image: Image = builder
+            .get_object("statusImage")
+            .expect("Glade source is missing statusImage");
+
+        let widget = Rc::new(SimilarityWidget {
+            button: button.clone(),
+            entry: entry.clone(),
+            inner: widget,
+            model: model.clone(),
+            status_image,
+        });
+
+        widget
+            .entry
+            .connect_changed(clone!(widget => move |_| widget.update_validity()));
+
+        entry.connect_activate(clone!(entry, model, widget => move |_| {
+            if model.embeddings().word_status(&widget.query()).is_valid() {
+        model.query(&entry.get_buffer().get_text());
+            }
+        }));
+
+        button.connect_clicked(
+            clone!(entry, model => move |_| model.query(&entry.get_buffer().get_text())),
+        );
+
+        widget.update_validity();
+
+        widget
+    }
+
+    fn query(&self) -> String {
+        self.entry.get_buffer().get_text()
+    }
+
+    fn update_validity(&self) {
+        let status = self.model.embeddings().word_status(&self.query());
+        self.button.set_sensitive(status.is_valid());
+
+        let icon_name = match status {
+            WordStatus::Known => "face-angel-symbolic",
+            WordStatus::Subword => "face-smile-symbolic",
+            WordStatus::Unknown => "face-crying-symbolic",
+        };
+
+        self.status_image
+            .set_from_icon_name(Some(icon_name), IconSize::LargeToolbar);
+    }
+
+    pub fn widget(&self) -> Box {
+        self.inner.clone()
+    }
+}
