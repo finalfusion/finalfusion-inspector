0b0113b044591fc401f1e01cb704f862e83806d0
Daniël de Kok <me@danieldk.eu>
2019-11-18 19:03:58 +0100

    Use Niv to manage dependencies
    This should make things a bit easier until Nix flakes are accepted and
    stabilized.

--- shell.nix
+++ shell.nix
@@ -1,20 +1,19 @@
 with import <nixpkgs> {};
 
 let
-  finalfusion = pkgs.callPackage (fetchFromGitHub {
-    owner = "finalfusion";
-    repo = "nix-packages";
-    rev = "a7fc91ec0336c04b0b51ba9c8c0a806427f96a42";
-    sha256 = "02dc1qz1zppi224jh0xlhc3k3qy3cpxcksp36c4cchcgfr0yz532";
-  }) {};
+  sources = import ./nix/sources.nix;
+  finalfusion = import sources.finalfusion {};
 in mkShell {
   nativeBuildInputs = [ qt5.qttools.dev python3Packages.autopep8 python3Packages.flake8 ];
 
   propagatedBuildInputs = [
+    (python3.withPackages (ps: with ps; [
+      pyqt5
+      setuptools
+      toml
+    ]))
+
     finalfusion.python3Packages.finalfusion
-    python3Packages.pyqt5
-    python3Packages.setuptools
-    python3Packages.toml
   ];
 
   # Normally set by the wrapper, but we can't use it in nix-shell (?).
