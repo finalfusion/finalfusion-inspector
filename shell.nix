0359b520d3183f0f88daa05e0cac79d04990e581
Daniël de Kok <me@danieldk.eu>
2020-06-08 13:18:13 +0200

    Use cargo and clippy from nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,16 +1,10 @@
 with import <nixpkgs> {};
 
-let
-  finalfusion = pkgs.callPackage (fetchFromGitHub {
-    owner = "finalfusion";
-    repo = "nix-packages";
-    rev = "94177ce11423a608af6969fcfafba323d89de16a";
-    sha256 = "1znhb79wj6ibrwa63nd3mm33k8s0a5s0ampcmfj8pajj41cfxdas";
-  }) {};
-in mkShell {
+mkShell {
   nativeBuildInputs = [
+    cargo
+    clippy
     gnome3.glade
-    latest.rustChannels.stable.rust
   ];
 
   buildInputs = [
